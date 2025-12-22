51378d181681e3ec07229dea4108a60994ee434a
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-22 14:38:57 +0600

    django is running

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from . import views
+urlpatterns = [
+    path('new/', views.new, name='new'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('contact/', views.contact, name='contact'),
+]
